5eb8f04a6d8cfa7452b6203dbf43e7829cd0bb32
Scott Franklin <stfranklin@wisc.edu>
2016-11-25 13:22:47 -0600

    Initial Commit for Web Application

---
+++ WebApplication/BatteryMotorMonitor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BatteryMotorMonitor.Startup))]
+namespace BatteryMotorMonitor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
